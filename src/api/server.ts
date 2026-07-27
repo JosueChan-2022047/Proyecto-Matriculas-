55acafe4cabc5f1b53bf9ab3c401cb0a5add95e3
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-07-27 02:29:50 -0600

    git commit -m "feat: Se agrego el server para las conexiones a postman"

---
+++ src/api/server.ts
@@ -0,0 +1,75 @@
+import http from "http";
+import { usuariosRouter } from "./usuariosRouter";
+import { vehiculosRouter } from "./vehiculosRouter";
+import { marcasRouter } from "./marcasRouter";
+import { tiposVehiculoRouter } from "./tiposVehiculoRouter";
+import { estadosMatriculaRouter } from "./estadosMatriculaRouter";
+import { matriculasRouter } from "./matriculasRouter";
+import { pagosRouter } from "./pagosRouter";
+import { documentosRouter } from "./documentosRouter";
+
+const PORT = 3000;
+
+export function iniciarServidor() {
+  const server = http.createServer(async (req, res) => {
+    try {
+      if (await usuariosRouter(req, res)) {
+        return;
+      }
+
+      if (await vehiculosRouter(req, res)) {
+        return;
+      }
+
+      if (await marcasRouter(req, res)) {
+        return;
+      }
+
+      if (await tiposVehiculoRouter(req, res)) {
+        return;
+      }
+
+      if (await estadosMatriculaRouter(req, res)) {
+        return;
+      }
+
+      if (await matriculasRouter(req, res)) {
+        return;
+      }
+
+      if (await pagosRouter(req, res)) {
+        return;
+      }
+
+      if (await documentosRouter(req, res)) {
+        return;
+      }
+
+      res.writeHead(404, {
+        "Content-Type": "application/json"
+      });
+
+      res.end(JSON.stringify({
+        mensaje: "Ruta no encontrada"
+      }));
+    } catch (error) {
+      console.error("Error en el servidor:", error);
+
+      if (!res.headersSent) {
+        res.writeHead(500, {
+          "Content-Type": "application/json"
+        });
+      }
+
+      res.end(JSON.stringify({
+        mensaje: "Error interno del servidor"
+      }));
+    }
+  });
+
+  server.listen(PORT, () => {
+    console.log(
+      `Servidor ejecutándose en http://localhost:${PORT}`
+    );
+  });
+}
